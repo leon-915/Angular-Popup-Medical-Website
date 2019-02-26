a3f77cd18bd631164afa744c66232ed8ffb37c8d
jchaconproximity <47428937+jchaconproximity@users.noreply.github.com>
2019-02-26 07:56:49 -0600

    Marketing Pages / Home Landing Page / UI
    Marketing Pages / Home Landing Page / UI

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+//import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+//import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.less']
 })
-export class NavMenuComponent implements OnInit {
+export class NavMenuComponent{
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  //isHandset:Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  //private breakpointObserver:BreakpointObserver
+  constructor(){}
 }
