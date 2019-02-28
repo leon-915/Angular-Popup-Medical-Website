c8f5de2435eed63c25c37cff0263342759c27157
Jorge Solano <jorgeasolano@outlook.com>
2019-02-28 07:37:34 -0600

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'https://w8xne1r4bk.execute-api.us-west-2.amazonaws.com/dev/'
+  apiURL: 'https://w8xne1r4bk.execute-api.us-west-2.amazonaws.com/dev/',
+  googleApiKey: 'AIzaSyCc7TDuzjxjjcDephVCoVRDH1qd-qtGzZs'
 };
