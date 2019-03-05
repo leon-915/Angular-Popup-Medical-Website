3ad20c57b1e71863d7438d7e3edf11bedc6fac03
Jorge Solano <jorgeasolano@outlook.com>
2019-03-04 20:05:36 -0600

    environment vars

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiURL: 'https://w8xne1r4bk.execute-api.us-west-2.amazonaws.com/test/',
+  googleApiKey: 'AIzaSyCc7TDuzjxjjcDephVCoVRDH1qd-qtGzZs',
+  recaptchaSiteKey: '6LemKJQUAAAAAGsjdXqZOqRl9ffbP5wP4TnxIKaP'
+};
