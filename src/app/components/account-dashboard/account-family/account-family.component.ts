8e3ef9a7c1e0b26be2a818fee4634dfb0722a5a9
Oscar Vargas <46572978+OscarVargasProximityCR@users.noreply.github.com>
2019-03-20 22:39:07 -0600

    MyFamilyUI changes in the screen

--- src/app/components/account-dashboard/account-family/account-family.component.ts
+++ src/app/components/account-dashboard/account-family/account-family.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormsModule
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService, NotificationService } from 'src/app/services';
+import { PasswordValidator } from 'src/app/validators';
+import { ReCaptchaV3Service } from 'ngx-captcha';
+
+import { SendPassResetConfirmationRequestModel } from 'src/app/models';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-account-family',
@@ -6,10 +19,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-family.component.less']
 })
 export class AccountFamilyComponent implements OnInit {
+  addMemberForm: FormGroup;
 
-  constructor() { }
+  addGuestMemberForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private accountSrv: AccountService,
+    private notificationSrv: NotificationService,
+    private reCaptchaV3Service: ReCaptchaV3Service,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.addMemberForm = this.fb.group(
+      {
+        email: ['', [Validators.required]]
+      },
+      {}
+    );
+    this.addGuestMemberForm = this.fb.group(
+      {
+        email: ['', [Validators.required]]
+      },
+      {}
+    );
   }
 
+  addMember() {}
+  addGuestMember() {}
+  removeGuestMember() {}
+
+  get email() {
+    return this.addMemberForm.get('email');
+  }
+  get memberEmail() {
+    return this.addGuestMemberForm.get('email');
+  }
 }
