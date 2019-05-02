5426ec599e2a3c7e6ee5431053fa35e03ec5f3ec
jchaconproximity <47428937+jchaconproximity@users.noreply.github.com>
2019-05-02 15:54:58 -0600

    loading page and UI fixes (join now and onboarding)
    loading page and UI fixes (join now and onboarding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -134,6 +134,7 @@ export function setupTranslateFactory(service: TranslateService) {
 }
 
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -189,7 +190,8 @@ import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
     TranslatorTestComponent,
     LanguageComponent,
     PaymentMethodsComponent,
-    AddMemberComponent
+    AddMemberComponent,
+    LoaderComponent
   ],
   imports: [
     AppRoutingModule,
